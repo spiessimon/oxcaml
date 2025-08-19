b13ded5224f4d2804609adcbca7138b7cd8242b0
Simon Spies <sspies@janestreet.com>
2025-08-19 18:40:12 +0100

    performance improvement and tweaks in file handling

--- backend/debug/dwarf/dwarf_ocaml/dwarf_type.ml
+++ backend/debug/dwarf/dwarf_ocaml/dwarf_type.ml
@@ -159,21 +159,21 @@ end = struct
     match d with
     | None -> ()
     | Some d ->
-      d.shape_size_before_reduction_memory <- Shape.size_in_memory shape;
+      d.shape_size_before_reduction_memory <- Obj.reachable_words (Obj.repr shape);
       d.shape_size_before_reduction_tree <- Shape.size shape
 
   let record_after_reduction d shape =
     match d with
     | None -> ()
     | Some d ->
-      d.shape_size_after_reduction_memory <- Shape.size_in_memory shape;
+      d.shape_size_after_reduction_memory <- Obj.reachable_words (Obj.repr shape);
       d.shape_size_after_reduction_tree <- Shape.size shape
 
   let record_after_evaluation d shape =
     match d with
     | None -> ()
     | Some d ->
-      d.shape_size_after_evaluation_memory <- Shape.size_in_memory shape;
+      d.shape_size_after_evaluation_memory <- Obj.reachable_words (Obj.repr shape);
       d.shape_size_after_evaluation_tree <- Shape.size shape
 
   let compute_die_size die =
@@ -1777,6 +1777,9 @@ module With_cms_reduce = Shape_reduce.Make (struct
      different variables. *)
 
   let read_unit_shape ~diagnostics ~unit_name =
+    (* CR sspies: We could consider throwing a louder error here, since
+    there must be something like a [.cms] version mismatch here. For
+    now, since it's only debugging information, we fail silently. *)
     match String.Tbl.find_opt cms_file_cache unit_name with
     | Some shape ->
       Shape_reduce.Diagnostics.count_cms_file_cached diagnostics;
@@ -1788,20 +1791,35 @@ module With_cms_reduce = Shape_reduce.Make (struct
       else (
         incr cms_files_read_counter;
         let filename = String.uncapitalize_ascii unit_name in
-        match Load_path.find_normalized (filename ^ ".cms") with
-        | exception Not_found -> None
-        | fn -> (
-          match Cms_format.read fn with
-          | exception Cms_format.Error _ ->
-            (* CR sspies: We could consider throwing a louder error here, since
-               there must be something like a [.cms] version mismatch here. For
-               now, since it's only debugging information, we fail silently. *)
-            None
-          | cms_infos ->
-            let shape = cms_infos.cms_impl_shape in
-            String.Tbl.add cms_file_cache unit_name shape;
-            Shape_reduce.Diagnostics.count_cms_file_loaded diagnostics;
-            shape))
+        let shape =
+          match Load_path.find_normalized (filename ^ ".cms") with
+          | exception Not_found -> (
+            (*= Format.eprintf "Warning: Cannot find CMS file %s.cms\n" filename; *)
+            match Load_path.find_normalized (filename ^ ".cmt") with
+            | exception Not_found ->
+              (*= Format.eprintf "Warning: Cannot find CMT file %s.cmt\n" filename; *)
+              None
+            | cmt_path -> (
+              match Cmt_format.read cmt_path with
+              | exception Cmt_format.Error _ ->
+                (*= Format.eprintf "Warning: Cannot read CMT file %s.cmt\n" filename; *)
+                None
+              | (_, Some cmt_infos) -> cmt_infos.cmt_impl_shape
+              | _ -> None))
+          | cms_path -> (
+            match Cms_format.read cms_path with
+            | exception Cms_format.Error _ ->
+              (*= Format.eprintf "Warning: Cannot read CMS file %s.cms\n" filename; *)
+              None
+            | cms_infos -> cms_infos.cms_impl_shape)
+        in
+        (*= (match Load_path.find_normalized (filename ^ ".cmi") with
+        | exception Not_found -> Format.eprintf "Warning: Cannot find CMI file\n"
+        | fn -> Format.eprintf "Found CMI file %s\n" fn); *)
+        (String.Tbl.add cms_file_cache unit_name shape;
+        Shape_reduce.Diagnostics.count_cms_file_loaded diagnostics;
+        shape) (* Note: This is an option, and we are also caching the None case. *)
+        )
 end)
 
 module D = Shape_reduction_diagnostics
