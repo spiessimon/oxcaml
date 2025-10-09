4e64c01b1189ecb586d1fb1ad58b0c82f2556c45
Simon Spies <sspies@janestreet.com>
2025-10-09 12:37:58 +0100

    disable mangling senstitive tests

--- oxcaml/tests/backend/zero_alloc_checker/gen/gen_dune.ml
+++ oxcaml/tests/backend/zero_alloc_checker/gen/gen_dune.ml
@@ -2,7 +2,9 @@ let () =
   let enabled_if =
     (* CR xclerc: on arm64, we currently get different results for a couple of
        cases (e.g. probes) *)
-    {|(enabled_if (and (= %{context_name} "main") (= %{architecture} "amd64")) )|}
+    (* Disable tests when RunLengthEncoding name mangling is enabled, since symbol names differ.
+       The OXCAML_NAME_MANGLING environment variable is set by the Makefile based on configure. *)
+    {|(enabled_if (and (= %{context_name} "main") (and (= %{architecture} "amd64") (= %{env:OXCAML_NAME_MANGLING=LegacyOCaml} "LegacyOCaml"))) )|}
   in
   let buf = Buffer.create 1000 in
   let print_test ?(extra_flags = "-zero-alloc-check default") deps =
